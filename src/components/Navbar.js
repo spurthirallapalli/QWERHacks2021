4af80738100d448a1991f0e7d8e1ca70e07b41fe
Natasha Puthukudy <nputhukudy@ucla.edu>
2020-09-25 00:42:42 -0700

    updated styling; use h1 and para on home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import "../index.css";
 import React from "react";
-import {Link } from "react-router-dom";
+import {NavLink } from "react-router-dom";
 
     function Navbar() {
 
@@ -8,23 +8,23 @@ import {Link } from "react-router-dom";
         
             <div>
                 <div class="buttons">
-                    <Link className = "text-link" to="/"><button type="button" id="about" class="b">home</button></Link>
-                    <Link className = "text-link" to="/apply"><button type="button" id="faq" class="b">apply!</button></Link>
-                    <Link className = "text-link" to="/faq"><button type="button" id="sched" class="b">faq</button></Link>
-                    <Link className = "text-link" to="/schedule"><button type="button" id="sandp" class="b">schedule</button></Link>
-                    <Link className = "text-link" to="/sponsors"><button type="button" id="resources" class="b">sponsors</button></Link>
-                    <Link className = "text-link" to="/mentors"><button type="button" id="resources" class="b">mentors</button></Link>
-                    <Link className = "text-link" to="/resources"><button type="button" id="resources" class="b">resources</button></Link>
+                    <NavLink exact className = "text-link" to="/"><button type="button" id="about" class="b">home</button></NavLink>
+                    <NavLink className = "text-link" to="/apply"><button type="button" id="faq" class="b">apply!</button></NavLink>
+                    <NavLink className = "text-link" to="/faq"><button type="button" id="sched" class="b">faq</button></NavLink>
+                    <NavLink className = "text-link" to="/schedule"><button type="button" id="sandp" class="b">schedule</button></NavLink>
+                    <NavLink className = "text-link" to="/sponsors"><button type="button" id="resources" class="b">sponsors</button></NavLink>
+                    <NavLink className = "text-link" to="/mentors"><button type="button" id="resources" class="b">mentors</button></NavLink>
+                    <NavLink className = "text-link" to="/resources"><button type="button" id="resources" class="b">resources</button></NavLink>
                 </div>
                 <div class="social_media">
                     <div class="icon">
-                        <a href="https://www.facebook.com/qwerhacks/" target="_blank"><img src={require("../img/fb-icon.png")} alt="facebook logo" width="40px"/></a>
+                        <a href="https://www.facebook.com/qwerhacks/" target="_blank"><img src={require("../img/fb-icon.png")} alt="facebook logo" width="30px"/></a>
                     </div>
                     <div class="icon">
-                        <a href="https://www.instagram.com/qwerhacks/" target="_blank"><img src={require("../img/ig-icon.png")} alt="instagram logo" width="40px"/></a>
+                        <a href="https://www.instagram.com/qwerhacks/" target="_blank"><img src={require("../img/ig-icon.png")} alt="instagram logo" width="30px"/></a>
                     </div>
                     <div class="icon"> 
-                        <a href="mailto:qwerhacks@gmail.com"><img src={require("../img/email-icon.png")} alt="email logo" width="40px"/></a>
+                        <a href="mailto:qwerhacks@gmail.com"><img src={require("../img/email-icon.png")} alt="email logo" width="30px"/></a>
                     </div>
                 </div>
             </div>
