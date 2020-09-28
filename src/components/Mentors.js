f39ffcfced7def62cf39bc586cb9c21f76867bc9
Spurthi <spurthi.rallapalli@gmail.com>
2020-09-28 11:05:51 -0700

    unused changes

--- src/components/Mentors.js
+++ src/components/Mentors.js
@@ -8,9 +8,13 @@ import React from "react";
               MENTORS
             </div>
             <div class="mentors">
-                <div>
-                  <img src={require("../img/mentortemplate.jpg")} alt="facebook logo" width="150px"/>
-                  <div>Here is information about mentor 1</div>
+                <div class="m">
+                    <img src={require("../img/mentortemplate.jpg")} alt="facebook logo" width="150px"/>
+                    <div class="mentor-text">
+                      <div class="mentor-name">Name</div>
+                      <div class="mentor-info">software engineer @ ucla elfin! she loves people, places, and things and is so happy to be here. you can ask her for help with anything, if she doesn’t know the answer to your question, she will point you in the direction of someone who does.</div>
+
+                    </div>
                 </div>
                 
             </div>
