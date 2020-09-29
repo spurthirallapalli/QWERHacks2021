08190423ac8af55b9fd81ee4313529417d8e2d74
Spurthi <spurthi.rallapalli@gmail.com>
2020-09-29 01:46:30 -0700

    Updates

---
+++ precache-manifest.c001efa59c6901cacc601f4f722c078d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424fb04b0cf6b8fa2652ba522568d3e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98c428869bca8e78d0ab",
+    "url": "/static/css/main.47c2532e.chunk.css"
+  },
+  {
+    "revision": "1e4f2cb39af1d653d21f",
+    "url": "/static/js/2.bb57af0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bb57af0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c428869bca8e78d0ab",
+    "url": "/static/js/main.266e0e96.chunk.js"
+  },
+  {
+    "revision": "a585c8e96928d892102d",
+    "url": "/static/js/runtime-main.68e7579c.js"
+  },
+  {
+    "revision": "bcf903ebf2d50269639b6aa8067dc703",
+    "url": "/static/media/qwerhacks_background.bcf903eb.png"
+  },
+  {
+    "revision": "c0e5731232f179f29806ad7ced0ffa82",
+    "url": "/static/media/track1.c0e57312.png"
+  },
+  {
+    "revision": "de1f09e9a7b1269e5ab43b347f9e62da",
+    "url": "/static/media/track2.de1f09e9.png"
+  },
+  {
+    "revision": "65e1eadaa2920f1bb768015c4e4dba73",
+    "url": "/static/media/track3.65e1eada.png"
+  },
+  {
+    "revision": "e7c5ef3df4b158fd372052500be73d50",
+    "url": "/static/media/track4.e7c5ef3d.png"
+  }
+]);
