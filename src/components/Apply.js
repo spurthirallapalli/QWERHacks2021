a561e9e9a1ec345a34200f4c7a6c0425283d3633
rithika <rithika.srinivasan@gmail.com>
2020-10-03 12:42:30 -0700

    embedded interest form on apply page

--- src/components/Apply.js
+++ src/components/Apply.js
@@ -5,7 +5,8 @@ import React from "react";
         return (
           <div>
             <h1>APPLY!</h1>
-            <p><b>coming soon...</b></p>
+            <p><b>fill out this interest form! registration will open in November </b></p>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdALkTq98s0N-a8QAHN7RiR4Y-OhH-8iF2v1HcOoTyKWFkFMQ/viewform?embedded=true" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
           </div>
         );
 
