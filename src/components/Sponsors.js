04f9a8fed24176d32a0dfdc092be357cdea610d1
Spurthi <spurthi.rallapalli@gmail.com>
2020-10-05 17:45:41 -0700

    updated partner logos

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -33,8 +33,8 @@ import React from "react";
             <h1>PARTNERS</h1>
             <div class="sponsor-class">
                 <div class="sponsors">
-                    <a class="spons-img" href="https://teachla.uclaacm.com" target="_blank"><img src={require("../img/teachLA_logo.png")} alt="teach LA logo" width="90px"/></a>
-                    <a class="spons-img" href="https://www.facebook.com/groups/uclaacmw" target="_blank"><img src={require("../img/acm_w_logo.png")} alt="acm-w logo" width="90px"/></a>
+                    <a class="spons-img" href="https://teachla.uclaacm.com" target="_blank"><img src={require("../img/acm-teachla-updated.png")} alt="teach LA logo" width="90px"/></a>
+                    <a class="spons-img" href="https://www.facebook.com/groups/uclaacmw" target="_blank"><img src={require("../img/acm-w-updated.png")} alt="acm-w logo" width="90px"/></a>
                 </div>
             </div>
           </div>
