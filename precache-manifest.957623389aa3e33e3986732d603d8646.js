2a422b1bb630cbe97576de0f3a6deef831773af5
Spurthi <spurthi.rallapalli@gmail.com>
2020-10-07 01:23:56 -0700

    Updates

---
+++ precache-manifest.957623389aa3e33e3986732d603d8646.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dde80b7c3d1af43624f16216dd58d7b",
+    "url": "/QWERHacks2021/index.html"
+  },
+  {
+    "revision": "7c43d34c24d04f5c4483",
+    "url": "/QWERHacks2021/static/css/main.8a8c24ea.chunk.css"
+  },
+  {
+    "revision": "17b13879b0a43827489b",
+    "url": "/QWERHacks2021/static/js/2.602e4fbe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/QWERHacks2021/static/js/2.602e4fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c43d34c24d04f5c4483",
+    "url": "/QWERHacks2021/static/js/main.e47fb606.chunk.js"
+  },
+  {
+    "revision": "51f012485ff8545e5b1d",
+    "url": "/QWERHacks2021/static/js/runtime-main.72c2181f.js"
+  },
+  {
+    "revision": "ff7d9d81aedf1f3645d01945b81adf5e",
+    "url": "/QWERHacks2021/static/media/holo-back.ff7d9d81.png"
+  },
+  {
+    "revision": "c0e5731232f179f29806ad7ced0ffa82",
+    "url": "/QWERHacks2021/static/media/track1.c0e57312.png"
+  },
+  {
+    "revision": "de1f09e9a7b1269e5ab43b347f9e62da",
+    "url": "/QWERHacks2021/static/media/track2.de1f09e9.png"
+  },
+  {
+    "revision": "65e1eadaa2920f1bb768015c4e4dba73",
+    "url": "/QWERHacks2021/static/media/track3.65e1eada.png"
+  },
+  {
+    "revision": "0a57fd22204b360d3d4d9bcadaf0e7a6",
+    "url": "/QWERHacks2021/static/media/track4-updated.0a57fd22.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6f8160971227168857ba0ede1a7b42c.js"
+  "/QWERHacks2021/precache-manifest.957623389aa3e33e3986732d603d8646.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/QWERHacks2021/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
