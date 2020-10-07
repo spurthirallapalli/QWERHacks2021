bcf469b4e9522eb39265ccf0d1110a61b85df299
Spurthi <spurthi.rallapalli@gmail.com>
2020-10-07 00:24:56 -0700

    fixed positioning of application form

--- src/components/Apply.js
+++ src/components/Apply.js
@@ -5,8 +5,8 @@ import React from "react";
         return (
           <div>
             <h1>APPLY!</h1>
-            <p><b>fill out this interest form! registration will open in November </b></p>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdALkTq98s0N-a8QAHN7RiR4Y-OhH-8iF2v1HcOoTyKWFkFMQ/viewform?embedded=true" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <div class="apply"><b>registration will open in November! in the meantime, fill out this interest form :) </b></div>
+            <iframe class="check" src="https://docs.google.com/forms/d/e/1FAIpQLSdALkTq98s0N-a8QAHN7RiR4Y-OhH-8iF2v1HcOoTyKWFkFMQ/viewform?embedded=true" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
           </div>
         );
 
