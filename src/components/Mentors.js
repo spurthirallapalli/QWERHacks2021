93ff00c6f7fa80bbbf45074e47594996906ecebf
Spurthi <spurthi.rallapalli@gmail.com>
2020-10-19 14:25:03 -0700

    added mentor

--- src/components/Mentors.js
+++ src/components/Mentors.js
@@ -13,19 +13,24 @@ import React from "react";
                     <div class="mentor-text">
                         <div><a href="http://raksha.gay" target="_blank" class="mentor-name">Raksha Muthukumar (she/her/hers)</a></div>
                         <div class="mentor-info">Raksha is a software engineer at Google by day and community organizer by night. she is passionate about building a tech future for the many, not just the few. Raksha organizes with DSA Tech Action, Manhattan Community Board 4, and through freelance writing.
-                        Ask Raskha about <b>Java, Typescript/Javascript, technical communication, webdev, data structures, resume feedback, git(hub)</b>
+                        Ask Raskha about <b>Java, Typescript/Javascript, technical communication, webdev, data structures, resume feedback, git(hub)!</b>
                         </div>
                     </div>
                 </div>
-                {/* <div class="people">
+                <div class="people">
                     <div class="mentor-image">
-                        <img src={require("../img/mentortemplate.jpg")} alt="facebook logo" width="160px"/>
+                        <img src={require("../img/moss.png")} alt="facebook logo" width="160px"/>
                     </div>
                     <div class="mentor-text">
-                        <div class="mentor-name">mentor 2 name</div>
-                        <div class="mentor-info">software engineer @ ucla elfin! she loves people, places, and things and is so happy to be here. you can ask her for help with anything, if she doesn’t know the answer to your question, she will point you in the direction of someone who does.</div>
+                        <div><a href="https://www.linkedin.com/in/annegail-moreland" target="_blank" class="mentor-name">Moss Moreland (they/them)</a></div>
+                        <div class="mentor-info">Moss is a software engineer at Travelers Insurance. they 
+                        recently graduated as a Cognitive Computer Science Major and are passionate 
+                        about solving complex problems for the greater benefit of human-kind. 
+                        they are perpetually working towards learning new skills, finding the best ways to teach others, 
+                        and building the future; ethics first. ask Moss about <b> machine learning, NLP, and algorithmic bias!</b>
+                         </div>
                     </div>
-                </div> */}
+                </div>
                 {/* <div class="people">
                     <div class="mentor-image">
                         <img src={require("../img/mentortemplate.jpg")} alt="facebook logo" width="160px"/>
