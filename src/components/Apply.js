b45848da51eee613c7b10835e4e9341783f57a8a
Spurthi <spurthi.rallapalli@gmail.com>
2020-11-01 00:08:28 -0700

    apply modifications

--- src/components/Apply.js
+++ src/components/Apply.js
@@ -5,7 +5,7 @@ import React from "react";
         return (
           <div>
             <h1>APPLY!</h1>
-            <div class="apply"><b>registration will open in November! in the meantime, fill out this interest form :) </b></div>
+            <div class="apply"><b>registration will open in November! in the meantime, fill out this interest form :) !! </b></div>
             <iframe class="check" src="https://docs.google.com/forms/d/e/1FAIpQLSdALkTq98s0N-a8QAHN7RiR4Y-OhH-8iF2v1HcOoTyKWFkFMQ/viewform?embedded=true" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
           </div>
         );
