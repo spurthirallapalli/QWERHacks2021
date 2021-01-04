8c5fcf5774762a65d6227720d4ee4a233e9795d8
Spurthi <spurthi.rallapalli@gmail.com>
2021-01-03 23:22:14 -0800

    schedule updates

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -13,35 +13,35 @@ import React from "react";
               </li>
               <li class="session">
                 <div class="when"><b>10:30-11:00 AM PST, sat 1/23</b></div>
-                <div class="topic">keynote</div>
+                <div class="topic">opening keynote with anna lytical</div>
               </li>
               <li class="session">
                 <div class="when"><b>11:00 AM - 12:00 PM PST, sat 1/23</b></div>
-                <div class="topic">track introductions</div>
+                <div class="topic">track introductions with kelly park, chris cuellar, ariel zucker, and james coleman</div>
               </li>          
               <li class="session">
                 <div class="when"><b>12:00 PM PST, sat 1/23</b></div>
-                <div class="topic">hacking begins</div>
+                <div class="topic"><b><i>hacking begins</i></b></div>
               </li> 
               <li class="session">
                 <div class="when"><b>12:00-1:00 PM PST, sat 1/23</b></div>
                 <div class="topic">“team formation &amp; first hackathon tips” and "git workshop" hosted by acm-w</div>
               </li>       
-              {/* <li class="session">
+              <li class="session">
                 <div class="when"><b>1:30-2:00 PM PST, sat 1/23</b></div>
-                <div class="topic">"building ethical technology" hosted by raksha</div>
+                <div class="topic">"building ethical technology" hosted by raksha muthukumar</div>
               </li>    
               <li class="session">
                 <div class="when"><b>2:00-2:30 PM PST, sat 1/23</b></div>
-                <div class="topic">raksha’s keynote track</div>
+                <div class="topic">"introduction to activism" by raksha muthukumar</div>
               </li>  
               <li class="session">
                 <div class="when"><b>3:00-3:30 PM PST, sat 1/23</b></div>
-                <div class="topic">"resume building/network workshop" hosted by moss</div>
+                <div class="topic">"resume building/network workshop" hosted by moss moreland</div>
               </li>  
               <li class="session">
                 <div class="when"><b>3:30-4:00 PM PST, sat 1/23</b></div>
-                <div class="topic">"navigating the workplace as nonbinary" hosted by moss</div>
+                <div class="topic">"navigating the workplace as nonbinary" hosted by moss moreland</div>
               </li> 
               <li class="session">
                 <div class="when"><b>4:30-5:00 PM PST, sat 1/23</b></div>
@@ -57,7 +57,7 @@ import React from "react";
               </li>  
               <li class="session">
                 <div class="when"><b>6:30-7:30 PM PST, sat 1/23</b></div>
-                <div class="topic">"Firebase Workshop" hosted by acm teach la</div>
+                <div class="topic">"firebase workshop" hosted by acm teach la</div>
               </li> 
               <li class="session">
                 <div class="when"><b>8:00-8:30 PM PST, sat 1/23</b></div>
@@ -67,19 +67,15 @@ import React from "react";
                 <div class="when"><b>8:30-9:00 PM PST, sat 1/23</b></div>
                 <div class="topic">"how to write a personal statement" hosted by qtstem</div>
               </li> 
-              <li class="session">
-                <div class="when"><b>9:00-9:30 PM PST, sat 1/23</b></div>
-                <div class="topic">MLH Activity</div>
-              </li>  */}
-                   
+              
             </ul>
             
-            {/* <ul>
+            <ul>
               <li class="session">
                 <div class="when"><b>12:30 PM PST, sun 1/24</b></div>
-                <div class="topic">hacking ends</div>
+                <div class="topic"><b><i>hacking ends</i></b></div>
               </li> 
-            </ul> */}
+            </ul>
             
 
 
