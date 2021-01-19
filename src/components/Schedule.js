3a6ba797af8a604869bfafda227d89346489a814
Spurthi <spurthi.rallapalli@gmail.com>
2021-01-18 23:25:01 -0800

    schedule updates

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -8,73 +8,141 @@ import React from "react";
             <h1><div>SCHEDULE</div> <div class="subtitle">// tentative</div></h1>
             <ul>
               <li class="session">
-                <div class="when"><b>10:00-10:30 AM PST, sat 1/23</b></div>
-                <div class="topic">opening presentations</div>
+                <div class="when"><b>10:00-11:00 AM PST, sat 1/23</b></div>
+                <div class="topic">opening presentations and keynote with anna lytical and the qwerhacks team</div>
               </li>
-              <li class="session">
-                <div class="when"><b>10:30-11:00 AM PST, sat 1/23</b></div>
-                <div class="topic">opening keynote with anna lytical</div>
-              </li>
+
+
               <li class="session">
                 <div class="when"><b>11:00 AM - 12:00 PM PST, sat 1/23</b></div>
                 <div class="topic">track introductions with kelly park, chris cuellar, ariel zucker, and james coleman</div>
-              </li>          
+              </li>  
+
+
               <li class="session">
                 <div class="when"><b>12:00 PM PST, sat 1/23</b></div>
                 <div class="topic"><b><i>hacking begins</i></b></div>
               </li> 
+
+
               <li class="session">
                 <div class="when"><b>12:00-1:00 PM PST, sat 1/23</b></div>
                 <div class="topic">“team formation &amp; first hackathon tips” and "git workshop" hosted by acm-w</div>
               </li>       
+
+              <li class="session">
+                <div class="when"><b>1:00-1:30 PM PST, sat 1/23</b></div>
+                <div class="topic">AT&amp;T sponsor takeover</div>
+              </li>   
+
+
               <li class="session">
                 <div class="when"><b>1:30-2:00 PM PST, sat 1/23</b></div>
                 <div class="topic">"building ethical technology" hosted by raksha muthukumar</div>
               </li>    
+
+
+              {/* Arlyn's workshop here */}
+
+
               <li class="session">
-                <div class="when"><b>2:00-2:30 PM PST, sat 1/23</b></div>
-                <div class="topic">"introduction to activism" by raksha muthukumar</div>
-              </li>  
+                <div class="when"><b>2:30-3:00 PM PST, sat 1/23</b></div>
+                <div class="topic">mentorship group meeting #1</div>
+              </li>    
+
               <li class="session">
                 <div class="when"><b>3:00-3:30 PM PST, sat 1/23</b></div>
                 <div class="topic">"resume building/network workshop" hosted by moss moreland</div>
               </li>  
+
               <li class="session">
                 <div class="when"><b>3:30-4:00 PM PST, sat 1/23</b></div>
                 <div class="topic">"navigating the workplace as nonbinary" hosted by moss moreland</div>
               </li> 
+
+              <li class="session">
+                <div class="when"><b>4:00-4:30 PM PST, sat 1/23</b></div>
+                <div class="topic">robinhood sponsor takeover</div>
+              </li>  
+
               <li class="session">
                 <div class="when"><b>4:30-5:00 PM PST, sat 1/23</b></div>
                 <div class="topic">"reactJS workshop" hosted by acm teach la</div>
               </li> 
+
               <li class="session">
                 <div class="when"><b>5:00-5:30 PM PST, sat 1/23</b></div>
                 <div class="topic">"intro to deep learning" hosted by acm ai</div>
               </li> 
+
+              <li class="session">
+                <div class="when"><b>5:30-6:00 PM PST, sat 1/23</b></div>
+                <div class="topic">xilinx sponsor takeover</div>
+              </li>  
+
               <li class="session">
                 <div class="when"><b>6:00-6:30 PM PST, sat 1/23</b></div>
-                <div class="topic">"intro to design and accessibility" hosted by bruin entrepreneurs</div>
-              </li>  
+                <div class="topic">"intro to design and accessibility" hosted by bruin entrepreneurs and all brains</div>
+              </li> 
+
+
               <li class="session">
-                <div class="when"><b>6:30-7:30 PM PST, sat 1/23</b></div>
+                <div class="when"><b>6:30-7:00 PM PST, sat 1/23</b></div>
                 <div class="topic">"firebase workshop" hosted by acm teach la</div>
               </li> 
+
+
+              <li class="session">
+                <div class="when"><b>7:00-7:30 PM PST, sat 1/23</b></div>
+                <div class="topic">"cybersecurity challenge" hosted by MLH</div>
+              </li> 
+
+              <li class="session">
+                <div class="when"><b>7:30-8:30 PM PST, sat 1/23</b></div>
+                <div class="topic">mentorship group meeting #2</div>
+              </li>  
+
               <li class="session">
                 <div class="when"><b>8:00-8:30 PM PST, sat 1/23</b></div>
                 <div class="topic">"how to be a better ally" hosted by qtstem</div>
               </li> 
+
               <li class="session">
                 <div class="when"><b>8:30-9:00 PM PST, sat 1/23</b></div>
                 <div class="topic">"how to write a personal statement" hosted by qtstem</div>
-              </li> 
-              
+              </li>               
             </ul>
             
             <ul>
+
               <li class="session">
-                <div class="when"><b>12:30 PM PST, sun 1/24</b></div>
+                <div class="when"><b>9:00-9:15 AM PST, sun 1/24</b></div>
+                <div class="topic">"pitching workshop" hosted by emily chung</div>
+              </li> 
+
+
+              <li class="session">
+                <div class="when"><b>12:00 PM PST, sun 1/24</b></div>
                 <div class="topic"><b><i>hacking ends</i></b></div>
               </li> 
+
+              <li class="session">
+                <div class="when"><b>12:30-2:00 PM PST, sun 1/24</b></div>
+                <div class="topic">judging</div>
+              </li> 
+
+
+              <li class="session">
+                <div class="when"><b>12:45-1:45 PM PST, sun 1/24</b></div>
+                <div class="topic">mentorship group meeting #3</div>
+              </li> 
+
+              <li class="session">
+                <div class="when"><b>2:15-3:00 PM PST, sun 1/24</b></div>
+                <div class="topic">closing speaker + keynote + awards</div>
+              </li>
+
+
             </ul>
             
 
